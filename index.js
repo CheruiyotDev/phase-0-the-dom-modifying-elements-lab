e43a4aca99a536171184c89d44bf5751379fae91
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-13 08:41:40 +0300

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "Evans is the champion"
+
+function removeElement() {
+  let main = document.getElementById("main")
+  main.remove()
+}
+removeElement()
